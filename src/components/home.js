7e47dc3ba2af044797475b7d9d99e04d4985c4d7
vinyasbj <vinyas555@gmail.com>
2019-01-22 22:39:51 +0530

    Home UI done

--- src/components/home.js
+++ src/components/home.js
@@ -8,27 +8,39 @@ class Home extends  React.Component {
         return(
             <div className="">
                <div className="ui grid button">
-               <img id="img" alt="Men's Outerwear" Style="opacity: 1; transition: opacity 0.5s ease 0s;" src="https://shop.polymer-project.org/esm-bundled/images/mens_outerwear.jpg"/>
+                    <img id="img" alt="Men's Outerwear" Style="opacity: 1; transition: opacity 0.5s ease 0s;" src="https://shop.polymer-project.org/esm-bundled/images/mens_outerwear.jpg"/>
                </div> 
                <div className = "ui equal width center aligned padded grid">
-               <div className="row ">
-                <div className="column ">
-                    <h4 className="item" Style= " margin-top: 3%;">Men's Outerwear</h4>
-                    <button className="ui inverted secondary button ">SHOP NOW</button>
+                <div className="row ">
+                    <div className="column ">
+                        <h4 className="item" Style= " margin-top: 3%;">Men's Outerwear</h4>
+                        <button className="ui inverted secondary button ">SHOP NOW</button>
+                    </div>
                 </div>
-               </div>
                </div>
                <div className="ui grid button">
-               <img id="img" alt="Men's Outerwear" Style="opacity: 1; transition: opacity 0.5s ease 0s;" src="https://shop.polymer-project.org/esm-bundled/images/ladies_outerwear.jpg"/>
+                    <img id="img" alt="Men's Outerwear" Style="opacity: 1; transition: opacity 0.5s ease 0s;" src="https://shop.polymer-project.org/esm-bundled/images/ladies_outerwear.jpg"/>
                </div> 
                <div className = "ui equal width center aligned padded grid">
-               <div className="row ">
-                <div className="column ">
-                    <h4 className="item" Style= " margin-top: 3%;">Ladies Outerwear</h4>
-                    <button className="ui inverted secondary button ">SHOP NOW</button>
+                <div className="row ">
+                    <div className="column ">
+                        <h4 className="item" Style= " margin-top: 3%;">Ladies Outerwear</h4>
+                        <button className="ui inverted secondary button ">SHOP NOW</button>
+                    </div>
                 </div>
                </div>
-               </div>
+               <div className="ui grid ">
+                <div className="eight wide column">
+                    <img id="img" alt="Men's Outerwear" Style="opacity: 1; transition: opacity 0.5s ease 0s;margin-bottom: 40px; outline: none;  float: left; width: 126%; height: 73%;" src="https://shop.polymer-project.org/esm-bundled/images/mens_tshirts.jpg"/>
+                    <h4 className="item" Style= " margin-top: 3%;">Ladies Outerwear</h4>
+                    <button className="ui inverted secondary button "Style= "margin-left: 42%;">SHOP NOW</button>
+                </div>
+                <div className="eight wide column">
+                    <img id="img" alt="Men's Outerwear" Style="opacity: 1; transition: opacity 0.5s ease 0s; margin-bottom: 40px; outline: none;  float: left; width: 126%; height: 73%;" src="https://shop.polymer-project.org/esm-bundled/images/ladies_tshirts.jpg"/>
+                    <h4 className="item" Style= " margin-top: 3%;">Ladies Outerwear</h4>
+                    <button className="ui inverted secondary button " Style= "margin-left: 42%;">SHOP NOW</button>
+                </div>
+            </div>
             </div>
         )
     }
