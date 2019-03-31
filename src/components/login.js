f0d2485020d015d59e9a3afd136c58f0131e2792
vinyasbj <vinyas555@gmail.com>
2019-03-31 22:45:45 +0530

    login function implemented

--- src/components/login.js
+++ src/components/login.js
@@ -5,16 +5,47 @@ import api from '../config/api';
 import axios from 'axios';
 
 class Login extends  React.Component {
-    componentDidMount(){
-        axios.get(`${api.tickets.baseUrl}/users/login`).then((response)=>{
-            this.setState({
-              products: response.data,
-            })
-            console.log('====================================')
-            console.log(this.state.products)
-            console.log('====================================')
-        })
+    constructor(props){
+      super(props);
+      this.state = {
+        email: "",
+        password: ""
       }
+      this.login = this.login.bind(this);
+      this.onChange = this.onChange.bind(this);
+    }
+
+    login(){
+      console.log('====================================')
+      console.log(this.state)
+      console.log("login fuction")
+      console.log('====================================')
+      axios.post(`${api.tickets.baseUrl}/users/login`, { email: "vinyas555@gmail.com",password: "12345678"}).then((response)=>{
+        
+          // return response.data
+          // console.log('====================================')
+          console.log(response.headers);
+          console.log(response.data);
+          
+    })
+    }
+
+    onChange(e){
+      this.setState({[e.target.name]: e.target.value})
+      // console.log( this.state)
+    }
+
+    // componentDidMount(){
+    //     axios.get(`${api.tickets.baseUrl}/users/login`).then((response)=>{
+    //         this.setState({
+    //           user: response.data,
+    //         })
+    //         console.log('====================================')
+    //         console.log(this.state.products)
+    //         console.log(response.headers);
+    //         console.log('====================================')
+    //     })
+    //   }
     render(){
         return <div >
          <div className="sixteen wide column" Style = {" text-align: center;"}><h1><b><Link to="/" Style="color: #0a0a0a"> Shop</Link></b></h1></div>
@@ -23,10 +54,10 @@ class Login extends  React.Component {
     <Grid columns={2} relaxed='very' stackable>
       <Grid.Column>
         <Form>
-          <Form.Input icon='user' iconPosition='left' label='Username' placeholder='Username' />
-          <Form.Input icon='lock' iconPosition='left' label='Password' type='password' />
+          <Form.Input icon='user' iconPosition='left'  name= "email" label='Username' placeholder='Username' onChange = {this.onChange} />
+          <Form.Input icon='lock' iconPosition='left'  name= " password" label='Password' type='password'  onChange = {this.onChange} />
 
-          <Button content='Login' primary />
+          <Button content='Login' onClick= {this.login} primary/>
         </Form>
       </Grid.Column>
 
